03a7e47d2a2ac4d0bf027993d5a32be5d22945d1
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-16 02:26:13 +0100

    Initial commit

---
+++ src/test/java/com/AlBarakaDigital/AlBarakaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlBarakaDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlBarakaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
