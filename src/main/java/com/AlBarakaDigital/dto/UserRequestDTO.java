f1a2cec3f7b9a09aa1e630eba3cd912611935b3b
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-18 14:06:53 +0100

    la création de dto UserRquestDTO

--- src/main/java/com/AlBarakaDigital/dto/UserRequestDTO.java
+++ src/main/java/com/AlBarakaDigital/dto/UserRequestDTO.java
@@ -1,4 +1,13 @@
 package com.AlBarakaDigital.dto;
 
+import com.AlBarakaDigital.enums.Role;
+import lombok.Data;
+
+@Data
 public class UserRequestDTO {
+    private String email;
+    private String password;
+    private String fullName;
+    private Role role;
 }
+
