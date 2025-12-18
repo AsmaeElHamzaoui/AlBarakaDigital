e7c1e7a5b51e10324ad502e519b65ac14c15ec70
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-18 17:11:48 +0100

    l'implémentation de dto AccountResponseDTO

--- src/main/java/com/AlBarakaDigital/dto/AccountResponseDTO.java
+++ src/main/java/com/AlBarakaDigital/dto/AccountResponseDTO.java
@@ -1,4 +1,12 @@
 package com.AlBarakaDigital.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class AccountResponseDTO {
+    private String accountNumber;
+    private BigDecimal balance;
 }
+
