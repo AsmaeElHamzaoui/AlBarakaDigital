6860ed257899a14cccbdb950d7c10933b0aac6c8
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-18 17:12:28 +0100

    l'implémentation du mapper AccountMapper

--- src/main/java/com/AlBarakaDigital/mapper/AccountMapper.java
+++ src/main/java/com/AlBarakaDigital/mapper/AccountMapper.java
@@ -1,4 +1,14 @@
 package com.AlBarakaDigital.mapper;
 
-public class AccountMapper {
+import com.AlBarakaDigital.dto.AccountResponseDTO;
+import com.AlBarakaDigital.entity.Account;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AccountMapper {
+
+    @Mapping(source = "owner.fullName", target = "owner")
+    AccountResponseDTO toDto(Account account);
 }
+
