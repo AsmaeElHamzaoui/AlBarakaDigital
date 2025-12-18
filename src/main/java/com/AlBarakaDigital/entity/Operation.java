07872fd12bbde5bc9ecaf8b41d3fac0da8c32041
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-18 17:26:43 +0100

    la création de l'entité Operation

--- src/main/java/com/AlBarakaDigital/entity/Operation.java
+++ src/main/java/com/AlBarakaDigital/entity/Operation.java
@@ -1,4 +1,49 @@
 package com.AlBarakaDigital.entity;
 
+import com.AlBarakaDigital.enums.OperationStatus;
+import com.AlBarakaDigital.enums.OperationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OperationType type;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private OperationStatus status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime validatedAt;
+    private LocalDateTime executedAt;
+
+    @ManyToOne
+    private Account accountSource;
+
+    @ManyToOne
+    private Account accountDestination;
+
+    @OneToOne(mappedBy = "operation", cascade = CascadeType.ALL)
+    private Document document;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
+
