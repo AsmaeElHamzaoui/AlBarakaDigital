2c231f94192d756110fb3ee5b3c15843eae393d4
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-19 15:44:08 +0100

    ABD-28: ticket terminé

--- src/main/java/com/AlBarakaDigital/controller/AgentOperationController.java
+++ src/main/java/com/AlBarakaDigital/controller/AgentOperationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//endpoints pour la gestion des operations par l'agent
 @RestController
 @RequestMapping("/api/agent/operations")
 @RequiredArgsConstructor
