1a453f32b242281ae4d1266f707f0262537df410
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-19 10:23:27 +0100

    laABD-11: 'implémentation de la classe CustomUserDetailsService (sous tache numéro 1 est terminée pour le ticket d'authentification

--- src/main/java/com/AlBarakaDigital/security/CustomUserDetailsService.java
+++ src/main/java/com/AlBarakaDigital/security/CustomUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.AlBarakaDigital.security;
 
-public class CustomUserDetailsService {
+import com.AlBarakaDigital.entity.User;
+import com.AlBarakaDigital.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Utilisateur introuvable"));
+
+        return new CustomUserDetails(user);
+    }
 }
