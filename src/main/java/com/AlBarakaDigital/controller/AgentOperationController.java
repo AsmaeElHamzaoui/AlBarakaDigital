23f73ee1a049808fdd136f6de65aad819868a91d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-19 15:36:34 +0100

    ABD-29: la'affichage des lites des operations avec l'état pending

--- src/main/java/com/AlBarakaDigital/controller/AgentOperationController.java
+++ src/main/java/com/AlBarakaDigital/controller/AgentOperationController.java
@@ -1,4 +1,22 @@
 package com.AlBarakaDigital.controller;
 
+import com.AlBarakaDigital.dto.OperationResponseDTO;
+import com.AlBarakaDigital.service.OperationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/agent/operations")
+@RequiredArgsConstructor
 public class AgentOperationController {
+
+    private final OperationService operationService;
+
+    @GetMapping("/pending")
+    public List<OperationResponseDTO> getPendingOperations() {
+        return operationService.getPendingOperations();
+    }
+
 }
