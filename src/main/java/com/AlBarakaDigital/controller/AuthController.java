5f85729d20a2f32b771dd3612a48f119115c66a1
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-19 12:20:12 +0100

    l'ABD-15: implémentation du controleur AuthController ppour traiter l'end point pour l'authentification

--- src/main/java/com/AlBarakaDigital/controller/AuthController.java
+++ src/main/java/com/AlBarakaDigital/controller/AuthController.java
@@ -1,4 +1,40 @@
 package com.AlBarakaDigital.controller;
 
+import com.AlBarakaDigital.dto.auth.*;
+import com.AlBarakaDigital.entity.User;
+import com.AlBarakaDigital.repository.UserRepository;
+import com.AlBarakaDigital.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String token = jwtUtil.generateToken(
+                user.getEmail(),
+                user.getRole().name()
+        );
+
+        return new LoginResponse(token);
+    }
 }
