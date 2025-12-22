718f58f34aac1a67a6b7873b6c98e28356d277c0
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-12-22 12:25:26 +0100

    la création de l'exception personnalisé AccountNotFoundException

---
+++ src/main/java/com/AlBarakaDigital/exception/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+
+package com.AlBarakaDigital.exception;
+
+
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
